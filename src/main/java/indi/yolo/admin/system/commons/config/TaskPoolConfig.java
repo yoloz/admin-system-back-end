962627c1d7498e58ad420b29831843936fe9d191
yoloz <youloz@outlook.com>
2023-11-08 16:42:58 +0800

    refactor: 添加异步线程池替换Async默认

---
+++ src/main/java/indi/yolo/admin/system/commons/config/TaskPoolConfig.java
@@ -0,0 +1,28 @@
+package indi.yolo.admin.system.commons.config;
+
+import org.springframework.aop.interceptor.AsyncExecutionAspectSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author yolo
+ */
+@Configuration
+public class TaskPoolConfig {
+
+    @Bean(name = AsyncExecutionAspectSupport.DEFAULT_TASK_EXECUTOR_BEAN_NAME)
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5); //核心线程池大小
+        executor.setMaxPoolSize(10); //最大线程数
+        executor.setQueueCapacity(200); //队列容量
+        executor.setKeepAliveSeconds(60); //活跃时间
+        executor.setThreadNamePrefix("dsg-web-"); //线程名字前缀
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy()); //哪个线程调用了execute方法，那么这个线程来执行被拒绝的任务
+        return executor;
+    }
+}
